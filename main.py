91edb71287832386ec844a4bf4d2d460b2069589
rudious01 <Rudious01@gmail.com>
2025-09-10 22:15:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+# List of sheer images (add more here)
+Sheer = [
+    "images/sheer1.jpg",
+    "images/sheer2.jpg",
+    "images/sheer3.jpg"
+]
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to sheer API! Go to /r-sheer to get a random sheer."}
+
+@app.get("/sheer")
+def get_sheer():
+    Sheer = random.choice(sheer)
+    return FileResponse(sheer)
